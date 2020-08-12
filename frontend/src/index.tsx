564ae8bed2dc2206a6cf1f94d67e96f84ba1a815
itolog <paukanlove@gmail.com>
2020-08-12 19:29:19 +0300

    'add_Apollo'

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import {config} from './shared/config/config'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const client = new ApolloClient({
+  uri: config.API_ENDPOINT,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ frontend/src/router/AppRouter.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import Cart from '../pages/Cart/Cart';
+import Shipping from '../pages/Shipping/Shipping';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/cart' />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/shipping'>
+          <Shipping />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
